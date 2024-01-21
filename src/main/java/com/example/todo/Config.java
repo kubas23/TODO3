d15610e5f2cf03c65bf6c27064f2768bdc601df1
jakub <jakubstanek38@gmail.com>
2024-01-21 13:28:03 +0100

    Change databse to MySQL

--- src/main/java/com/example/todo/Config.java
+++ src/main/java/com/example/todo/Config.java
@@ -15,7 +15,7 @@ public class Config {
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/todo_app");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/todo_3");
         dataSource.setUsername("root");
         dataSource.setPassword("6154");
         return dataSource;
