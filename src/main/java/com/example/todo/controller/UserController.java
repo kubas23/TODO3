93e77fc4aca8f612e8abae886e41780b2478f999
jakub <jakubstanek38@gmail.com>
2024-02-03 14:42:01 +0100

    Backend of authentification user

--- src/main/java/com/example/todo/controller/UserController.java
+++ src/main/java/com/example/todo/controller/UserController.java
@@ -1,17 +1,41 @@
 package com.example.todo.controller;
 
+import com.example.todo.model.User;
+import com.example.todo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 
     @GetMapping("/login")
-    public String login(){
+    public String loginPage() {
         return"login";
+    } @GetMapping("/login")
+    public String loginPageRedirect() {
+        return "register";
     }
 
-    @GetMapping("/register")
-    public String register(){
+    @GetMapping("/registration")
+    public String registrationForm(Model model){
+        model.addAttribute("user", new User());
         return"register";
     }
+
+    @PostMapping("/registration")
+    public String registrationSubmit(@ModelAttribute("user") User user){
+        userService.registerUser(user);
+        return "redirect:/login"; // Presmeruje na login page po uspesne registraci
+    }
+
 }
---
+++ src/main/java/com/example/todo/model/User.java
@@ -0,0 +1,18 @@
+package com.example.todo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+}
