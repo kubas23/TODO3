460ebd3f54e7c5364f39057c0068dcadb121a09a
Simon <cluecz@gmail.com>
2024-02-10 10:11:35 +0100

    test fix

--- src/test/java/com/example/todo/ToDoApplicationTests.java
+++ src/test/java/com/example/todo/ToDoApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.todo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(classes = ToDoApplicationTests.class)
 class ToDoApplicationTests {
 
     @Test
